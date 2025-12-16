27691d98e5a4aea5a8654269a5bc6f43f51f305c
christian <pariscachristian@gmail.com>
2025-12-16 10:29:35 -0400

    refactor: Use spread operator for cleaner prop handling

--- src/components/ui/Atoms/Input.tsx
+++ src/components/ui/Atoms/Input.tsx
@@ -1,26 +1,26 @@
-interface InputProps {
-  type?: string;
+import React, { type InputHTMLAttributes } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   id: string;
   name: string;
-  placeholder?: string;
-  accept?: string;
-  required?: boolean;
   value?: string | number;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function Input({ type, id, name, placeholder, accept ,value, onChange, required = true }: InputProps) {
+export default function Input({
+  type = "text",
+  value,
+  onChange,
+  ...rest
+}: InputProps) {
+
   return (
     <input
-      type={type || "text"}
-      id={id}
-      name={name}
-      required = {required}
-      placeholder={placeholder}
-      accept={accept}
+      type={type}
       value={value}
       onChange={onChange}
       className="w-full p-2 inset-0  border-gray-400 rounded-md border focus:outline-none focus:ring-2 focus:ring-teal-600 focus:border-teal-600"
+      {...rest}
     />
   );
 }
--- src/components/ui/Atoms/Textarea.tsx
+++ src/components/ui/Atoms/Textarea.tsx
@@ -1,22 +1,21 @@
-interface TextareaProps {
+import React, {type TextareaHTMLAttributes} from "react";
+
+interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement>{
     id?: string;
     name?: string;
-    placeholder: string;
     value?: string;
     onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-export default function Textarea({id, name , placeholder, value, onChange}: TextareaProps) {
+export default function Textarea({id, name , value, onChange, ...rest}: TextareaProps) {
   return (
     <textarea
       id={id}
       name={name}
-      rows={5}
-      placeholder={placeholder}
-      required
       value={value}
       onChange={onChange}
       className="w-full p-2 border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-600 focus:border-teal-600 resize-none"
+      {...rest}
     />
   );
 }
